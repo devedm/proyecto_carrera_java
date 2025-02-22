b5b00a1aeb0716c5e2c93dc6a80f5e1079e81dca
FernandaFT <nanda199784@gmail.com>
2025-02-22 15:57:31 -0600

    adding new files

---
+++ src/main/java/com/mycompany/proyectofinalgrupo8/Nodo.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectofinalgrupo8;
+
+/**
+ *
+ * @author fernandafajardo
+ */
+public class Nodo {
+    
+    String operacion;
+    int numero;
+    Nodo sig;
+
+    public Nodo() {
+    }
+
+    public Nodo(String operacion, int numero) {
+        this.operacion = operacion;
+        this.numero = numero;
+        this.sig = null;
+    }
+
+    public String getOperacion() {
+        return operacion;
+    }
+
+    public void setOperacion(String operacion) {
+        this.operacion = operacion;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Nodo getSig() {
+        return sig;
+    }
+
+    public void setSig(Nodo sig) {
+        this.sig = sig;
+    }
+    
+    
+    
+}
---
+++ src/main/java/com/mycompany/proyectofinalgrupo8/PilaGenerica.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectofinalgrupo8;
+
+/**
+ *
+ * @author fernandafajardo
+ */
+public class PilaGenerica {
+    
+    private Nodo top;
+    
+    public PilaGenerica() {
+        top = null;
+    }
+
+    public Nodo getTop() {
+        return top;
+    }
+
+    public void setTop(Nodo top) {
+        this.top = top;
+    }
+    
+    /***
+     * Agrega un nuevo nodo a la pila con la operación y el número especificados.
+     * @param operacion Nombre o descripción de la operación asociada al nodo.
+     * @param numero Valor numérico asociado al nodo.
+     */
+    public void push(String operacion, int numero){
+        
+        Nodo miNodo = new Nodo(operacion, numero);
+        if(isEmpty()){
+            top = miNodo;
+        }else{
+            miNodo.setSig(top);
+            top = miNodo;
+        }
+    }
+    
+    /**
+     * Elimina y devuelve el nodo del top de la pila.
+     * @return El nodo eliminado de la pila. Retorna {@code null} si la pila está vacía.
+     */
+    public Nodo pop(){
+        if(isEmpty()){
+            System.out.println("Error: la pila esta vacía, no puede desapilar");
+            return null;
+        }else{
+            Nodo aux = top;
+            top = top.getSig();
+            return aux;
+        }
+    }
+    
+    /**
+     * Verifica si la pila esta vacía.
+     * @return {@code true} si la pila no tiene elementos, {@code false} en caso contrario.
+     */
+    public boolean isEmpty(){
+        
+        if(top == null){
+            return true;
+        }else{
+           return false;
+        }
+    }
+}
