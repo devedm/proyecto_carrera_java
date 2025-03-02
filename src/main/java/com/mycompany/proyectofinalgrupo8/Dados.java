94d45788ff9669f4476b15717b2f87887d8a44c7
a1mp <ignaciozp21@gmail.com>
2025-03-02 01:57:36 -0600

    Agregue los getters y setters de las pilas anteriormente mencionadas

--- src/main/java/com/mycompany/proyectofinalgrupo8/Dados.java
+++ src/main/java/com/mycompany/proyectofinalgrupo8/Dados.java
@@ -22,6 +22,25 @@ public class Dados {
     public void tirar(){
         setValorDado1((int)(Math.random() * 6) + 1);
         setValorDado2((int)(Math.random() * 6) + 1);
+        int sumaDados = getValorDado1() + getValorDado2();      // Sumo los resultados para comparatlos
+        Jugador temp = colaAux.desencolar();                    // Desencolar devuelva un juagor lo guardo en una variable temporal
+
+        if (sumaDados%2 == 0) {
+            int premioObtenido = premios.getTop().getNumero();
+            System.out.println("Obtuviste un número par, debes tomar un premio de la pila. Mucha Suerte");
+            System.out.println("Como premio puedes moverte: " + premioObtenido + " veces");
+            System.out.println("Seras movido a la posicion: " + (temp.getPosicion() + premioObtenido));
+            temp.moverJugador(premioObtenido);
+        } else {
+            int castigoObtenido = castigos.getTop().getNumero();
+            System.out.println("Obtuviste un número impar, debes tomar un castigo de la pila. Mejor suerte la próxima vez");
+            System.out.println("Como castigo retrocederas: " + castigoObtenido + " veces");
+            System.out.println("Seras movido a la posicion: " + (temp.getPosicion() + castigoObtenido));
+            temp.moverJugador(castigoObtenido);
+        }
+
+        System.out.println("La posicion actual del jugador es: " + temp.getPosicion());
+        colaAux.encolar(temp);          // Se encola a la ultima posicion al jugador encolado
     }
     
     /**
@@ -68,6 +87,24 @@ public class Dados {
      */
     public void setValorDado2(int valorDado2) {
         this.valorDado2 = valorDado2;
+    }
+
+
+
+    public PilaPremios getPremios() {
+        return premios;
+    }
+
+    public void setPremios(PilaPremios premios) {
+        this.premios = premios;
+    }
+
+    public PilaCastigos getCastigos() {
+        return castigos;
+    }
+
+    public void setCastigos(PilaCastigos castigos) {
+        this.castigos = castigos;
     }
 }
 
